1254bccfb96ebfd2ae3d011283d66b06c5cac6ba
mbinney <mbinney@westfieldhealth.com>
2017-05-22 16:47:49 +0100

    first commit

---
+++ Applications.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Office
+{
+    partial class Applications
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExcel = new System.Windows.Forms.Button();
+            this.btnWord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Location = new System.Drawing.Point(12, 12);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(79, 32);
+            this.btnExcel.TabIndex = 0;
+            this.btnExcel.Text = "Excel";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnWord
+            // 
+            this.btnWord.Location = new System.Drawing.Point(97, 12);
+            this.btnWord.Name = "btnWord";
+            this.btnWord.Size = new System.Drawing.Size(85, 31);
+            this.btnWord.TabIndex = 1;
+            this.btnWord.Text = "Word";
+            this.btnWord.UseVisualStyleBackColor = true;
+            this.btnWord.Click += new System.EventHandler(this.btnWord_Click);
+            // 
+            // Applications
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(203, 58);
+            this.Controls.Add(this.btnWord);
+            this.Controls.Add(this.btnExcel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Applications";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Office Applications";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExcel;
+        private System.Windows.Forms.Button btnWord;
+    }
+}
+
---
+++ Applications.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Word;
+
+namespace Office
+{
+    public partial class Applications : Form
+    {
+        public Applications()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            build_excel_file();
+            MessageBox.Show("Finished");
+            
+
+        }
+
+        private void build_excel_file()
+        {
+
+            Microsoft.Office.Interop.Excel.Application objApp;                
+            Workbook objWB;
+            Worksheet objWS;
+            string filename = "c:\\temp\\interop.xlsx";
+            object misValue = System.Reflection.Missing.Value;
+
+            //objApp = new Microsoft.Office.Interop.Excel.ApplicationClass();
+            objApp = new Microsoft.Office.Interop.Excel.Application();
+
+            objWB = objApp.Workbooks.Add();
+            objWS = (Worksheet)objWB.Sheets[1];
+
+            objWS.Select();
+
+            objWS.Cells[1, 1] = "First Name";
+            objWS.Cells[1, 2] = "Last Name";
+            objWS.Cells[1, 3] = "Full Name";
+            objWS.Cells[1, 4] = "Salary";
+                       
+            objApp.Visible = false;
+
+            objWB.Close(true, filename, misValue);
+            //objWB.Close(0); -- no save changes.
+
+            //http://stackoverflow.com/questions/8977571/excel-process-remains-open-after-interop-traditional-method-not-working
+            //These will close the excel object and force garbage collection.  Watch Task manager. Keep on eye on the Excel process.
+            objApp.Quit();
+            if (objWB != null) { Marshal.ReleaseComObject(objWB); } //release each workbook like this
+            if (objWS != null) { Marshal.ReleaseComObject(objWS); } //release each worksheet like this
+            if (objApp != null) { Marshal.ReleaseComObject(objApp); } //release the Excel application
+            objWB = null; //set each memory reference to null.
+            objWS = null;
+            objApp = null;
+            GC.Collect();
+
+
+            
+
+        }
+
+        public void build_word_doc() {
+
+            //http://www.c-sharpcorner.com/article/word-automation-using-C-Sharp/
+            object misValue = System.Reflection.Missing.Value;
+
+            Microsoft.Office.Interop.Word.Application objApp = new Microsoft.Office.Interop.Word.Application();
+            Microsoft.Office.Interop.Word.Document objDoc = new Microsoft.Office.Interop.Word.Document();
+
+            objApp.Documents.Add(misValue, misValue, misValue, misValue);
+            
+            objApp.Visible = true;
+
+            
+            if (objApp != null) { Marshal.ReleaseComObject(objApp); }
+            if (objDoc != null) { Marshal.ReleaseComObject(objDoc); }
+
+            //http://stackoverflow.com/questions/8977571/excel-process-remains-open-after-interop-traditional-method-not-working
+            //These will close the excel object and force garbage collection.  Watch Task manager. Keep on eye on the Excel process.
+            objApp = null;
+            objDoc = null;
+            GC.Collect();
+
+
+        }
+
+        private void btnWord_Click(object sender, EventArgs e)
+        {
+            build_word_doc();
+        }
+    }
+}
